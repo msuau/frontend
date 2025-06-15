e772dca6c0458bbbc219ee03a29df7dfe24196ba
msuau <mat.suau@orange.fr>
2025-06-16 00:34:08 +0200

    fix for click event handling

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const API_URL = 'https://api-blagues-carambar.onrender.com/api/v1/jokes/random';
 jokeBtn.addEventListener('click', async () => {
 jokeDisplay.textContent = 'Chargement de la blague...';
 try {
-const res = await fetch(API_URL);
-const joke = await res.json();
-jokeDisplay.textContent = joke.content + '&bsp'  + joke.answer || 'Oops ! Blague introuvable.';
+const response = await fetch(API_URL);
+const joke = await response.json();
+jokeDisplay.textContent = joke.content + '&bsp'  + joke.answer  || 'Oops ! Blague introuvable.';
 } catch (error) {
 console.error(error);
 jokeDisplay.textContent = 'Erreur lors du chargement de la blague.';
